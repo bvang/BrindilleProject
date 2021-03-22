fa0486ff4e774e3bd5711184e24f49c44227b2b8
bvang <billy.vang27@gmail.com>
2021-03-22 14:32:56 +0100

    import info from file

---
+++ main.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+from pymediainfo import MediaInfo
+import os, shutil
+
+media_info = MediaInfo.parse("C:/Users/sasuk/Downloads/J13_21082020.mp4")
+for track in media_info.tracks:
+    if track.track_type == "Video":
+        print("Bit rate: {t.bit_rate}, Frame rate: {t.frame_rate}, "
+              "Format: {t.format}".format(t=track)
+        )
+        print("Duration (raw value):", track.duration)
+        print("Duration (other values:")
+        pprint(track.other_duration)
+    elif track.track_type == "Audio":
+        print("Track data:")
+        pprint(track.to_data())
+
+os.chdir("C:/Users/sasuk/Downloads/")
+#test
